1e77307052f681158306815f6a9b6a63c42e894c
Igor Swatowski <igi66@wp.pl>
2021-01-02 17:57:27 +0100

    Add first structure files look and dependencies like axios, styled-components..

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Root from './views/Root';
+import Menu from './components/Menu/Menu';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
